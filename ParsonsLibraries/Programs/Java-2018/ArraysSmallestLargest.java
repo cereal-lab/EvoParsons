c0f6238fa022877031822ec755b6c643d7378eee
System Administrator <root@USF-Gold-Wifi-226-12-207.laptops.usf.edu>
2019-03-26 17:38:56 -0400

    fixing bugs for two Java programs after automatic compilation script

--- ParsonsLibraries/Programs/Java-2018/ArraysSmallestLargest.java
+++ ParsonsLibraries/Programs/Java-2018/ArraysSmallestLargest.java
@@ -16,12 +16,12 @@ Implement these method in the following order; main, displaySmallestLargest, get
 
 [#source#]
 */
-import java.util.Scanner; \\npublic class ArraysSmallestLargest { \\n public static void main(String[] args){ \\n int size = 10; \\n int[] data = getRandomArray(size); \\n displayLargestSmallest(data); \\n } //end of main method
+import java.util.Scanner; \\npublic class ArraysSmallestLargest { \\n public static void main(String[] args){ \\n int size = 10; \\n int[] data = getRandomArray(size); \\n displaySmallestLargest(data); \\n } //end of main method
    public static void displaySmallestLargest(int[] array){
       int smallest = array[0], largest = array[0];
       for(int i = 1 ; i < array.length ; i++){
-         if(array[i] > largestValue) { \\n largestValue = array[i]; \\n }
+         if(array[i] > largest) { \\n largest = array[i]; \\n }
          else if(array[i] < smallest) { \\n smallest = array[i]; \\n }
-      }//end of for loop inside displayLargestSmallest
+      }//end of for loop inside displaySmallestLargest
       System.out.println("Largest is " + largest + ", smallest is " + smallest);\\n} // end of displaylargestSmallest method
-   public static int[] getRandomArray(int size){\\nint[] data = new int[size];\\nfor(int i = 0 ; i < data.length ; ){ \\n  data[i++] = (int)(Math.random()*max+1);\\n } \\nreturn data;\\n} // end of getRandomArray method\\n} // end of class
+   public static int[] getRandomArray(int size){\\nint[] data = new int[size];\\nfor(int i = 0 ; i < data.length ; ){ \\n  data[i++] = (int)(Math.random()*10 + 1);\\n } \\nreturn data;\\n} // end of getRandomArray method\\n} // end of class
